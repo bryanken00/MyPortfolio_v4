a1ebfcceeb46ba47e9155c1921765171ab2380f0
bryanken00 <bryanken01230@gmail.com>
2024-03-04 11:50:54 +0800

    fix header

--- src/Shared/Components/Partials/Header.jsx
+++ src/Shared/Components/Partials/Header.jsx
@@ -23,9 +23,9 @@ const Header = () => {
 
   const textChanger = () => {
     if (isScrolled) {
-      return headercolorScrolled
+      return '#ffffff'
     } else {
-      return headercolorNotScrolled
+      return '#1F1717'
     }
   }
 
@@ -36,11 +36,12 @@ const Header = () => {
   return (
     <header className={`bg-custom-headerColorBG fixed w-full py-4 px-10 ${isScrolled ? ' shadow-md bg-cyan-700' : 'bg-cyan-700 bg-opacity-0'}`} style={{ transition: 'background-color 0.3s' }}>
       <nav className="container mx-auto flex flex-col lg:flex-row lg:justify-between items-center">
-        <a href="/" className={`text-xl text-${textChanger()} font-bold mb-4 lg:mb-0 ${transition()}`}>Home</a>
+        {console.log(textChanger())}
+        <a href="/" className={`text-xl text-[${textChanger()}] font-bold mb-4 lg:mb-0 ${transition()}`}>Home</a>
         <div>
-          <a href="/Project" className={`text-${textChanger()} hover:text-black mr-4`}>Projects</a>
-          <a href="/Resume" className={`text-${textChanger()} hover:text-black-300 mr-4`}>Resume</a>
-          <a href="/Contact" className={`text-${textChanger()} hover:text-black-300`}>Contact</a>
+          <a href="/Project" className={`text-[${textChanger()}] hover:text-black mr-4 ${transition()}`}>Projects</a>
+          <a href="/Resume" className={`text-[${textChanger()}] hover:text-black-300 mr-4 ${transition()}`}>Resume</a>
+          <a href="/Contact" className={`text-[${textChanger()}] hover:text-black-300 ${transition()}`}>Contact</a>
         </div>
       </nav>
     </header>
